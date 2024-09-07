7e69d77ced644f8610eefba0aa9dd0b25027524b
danikarius <daniellacarius@gmail.com>
2024-09-06 21:22:20 -0300

    subindo lista 4 - primeira parte

---
+++ Exercicio1.java
@@ -0,0 +1,35 @@
+package com.poo.listas.logica4.lista4;
+
+import java.util.Scanner;
+
+public class Exercicio1 {
+
+    /*1) Voce é gerente de um supermercado e sabe que os valores das
+         vendas do dia são gravados em um vetor. Digite um código que faça o
+         fechamento (soma) de vendas do dia de maneira automatizada.*/
+
+         public static void resolucao4()throws InterruptedException {
+    
+            Scanner scanner = new Scanner(System.in);
+    
+            System.out.println("Número de vendas do dia: ");
+            int numeroVendas = scanner.nextInt();
+    
+            double vendas[] = new double [numeroVendas];
+            double totalVendas = 0;
+    
+            for (int i = 0; i < numeroVendas; i++) {
+                System.out.println("Digite o valor da venda " + (i + 1) + ":");
+                vendas[i] = scanner.nextDouble();
+            }
+            
+            for (double venda : vendas) {
+                totalVendas += venda;
+            }
+            System.out.println("Total das vendas do dia: R$" + totalVendas);
+
+            scanner.close();
+        }
+}
+
+
---
+++ Lista4Application.java
@@ -0,0 +1,15 @@
+package com.poo.listas.logica4.lista4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lista4Application {
+
+	public static void main(String[] args) throws InterruptedException {
+		SpringApplication.run(Lista4Application.class, args);
+		
+		Exercicio1.resolucao4();
+	}
+	
+}
